aea5a54fe42774d4cd501a0c276eb18588a23e4e
engladian <simon.franklin2@cigna.com>
2015-10-01 01:32:57 +1300

    Completed

--- app/app.js
+++ app/app.js
@@ -1,12 +1,47 @@
 var calendarModule = angular.module('calendarDemoApp', []);
 
 // your controller and directive code go here
+calendarModule.controller('calenderController',
+['$scope', 'calenderFactory', function ($scope, calenderFactory) {
+    $scope.selectedMonthYear = new Date();
+    function setCalender() {
+        $scope.calenderMap = calenderFactory.createCalenderMap
+                                              ($scope.selectedMonthYear);
+    }
+    setCalender();
 
-calendarModule.controller('dateSelectController', ['$scope', function ($scope) {
-
-    $scope.dateSelect = {};
-
-    var curMonth = new Date().getMonth();
-    var curYear = new Date().getFullYear();
-    $scope.dateSelect.monthYear = new Date(curYear, curMonth, 1);
+    /////////////////////////////////////////////////////////
+    $scope.buildCalender = function() {
+        setCalender();
+    };
 }]);
+
+calendarModule.directive('calender', function () {
+    return {
+        restrict: 'E',
+        templateUrl: './calender-directive/calender.html'
+    };
+});
+
+calendarModule.factory('calenderFactory', function() {
+    return {
+        createCalenderMap: function(date) {
+            var selCalRange = CalendarRange.getMonthlyRange(date);
+            var curMonth = date.getMonth();
+            var calDisplayMap = [];
+            var calDisplayRow = [];
+            for (var idx = 0; idx < 35; idx++) {
+                calDisplayRow.push(
+                    { 
+                        day:         selCalRange.days[idx].day, 
+                        otherClass:  selCalRange.days[idx].month !== curMonth  
+                    });
+                if (selCalRange.days[idx].date.getDay() === 6) {
+                    calDisplayMap.push(calDisplayRow);
+                    calDisplayRow = [];
+                }
+            }
+            return calDisplayMap;
+        }
+    }
+});
